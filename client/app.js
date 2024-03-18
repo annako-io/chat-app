8400d9bb5d17e76ea3d1704d4f0e738265ccd343
annako-io <annako1@icloud.com>
2024-03-17 17:47:08 -0700

    basic messaging for multiple users

--- client/app.js
+++ client/app.js
@@ -1,10 +1,27 @@
 const socket = io('ws://localhost:3500');
 
 // Global variables
+const msgInput = document.querySelector('#message');
+const chatDisplay = document.querySelector('.chat-display');
 
 // Send message function
-const sendMessage = (e) => {};
+const sendMessage = (e) => {
+  e.preventDefault();
+
+  if (msgInput.value) {
+    socket.emit('message', msgInput.value);
+    msgInput.value = '';
+  }
+  msgInput.focus();
+};
 
 // Event listeners
+document.querySelector('.form-msg').addEventListener('submit', sendMessage);
 
 // Message list
+socket.on('message', (data) => {
+  const post = document.createElement('li');
+  post.className = 'post';
+  post.innerHTML = data;
+  document.querySelector('.chat-display').appendChild(post);
+});
--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const { Server } = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3500;
+const ADMIN = 'Admin';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 const expressServer = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
@@ -16,4 +17,22 @@ const io = new Server(expressServer, {
   cors: {
     origin: process.env.NODE_ENV === 'production' ? false : ['http://localhost:5500', 'http://127.0.0.1:5500']
   }
+});
+
+// User connects to app
+io.on('connection', socket => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.emit('message', 'You have joined the Chat App!');
+});
+
+// User disconnects
+socket.on('disconnect', () => {
+  const user = getUser(socket.id);
+  console.log(`User ${socket.id} disconnected`);
+});
+
+// Listen for a message event
+socket.on('message', data => {
+  io.emit('message', data);
 });
